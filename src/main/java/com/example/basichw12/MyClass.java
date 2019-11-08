ae1eecd43a0a3201963f1c39ab6984ef5d56ddc6
shanglin10 <a0926753805@gmail.com>
2019-11-08 17:48:07 +0800

    1081108update

---
+++ src/main/java/com/example/basichw12/MyClass.java
@@ -0,0 +1,22 @@
+package com.example.basichw12;
+
+
+import java.util.Scanner;
+
+public class MyClass {
+    public static void main ( String[] args ) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter your square's length");
+        int n = scanner.nextInt();
+        for(int i=n;i>0;i--){
+            for(int k=n-i ;k>0;k--){
+                System.out.print(" ");
+            }
+            for (int j=i;j>0;j--){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+    }
+}
